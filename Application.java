cb0523fde9ee6867623ae7e5f0eaac0f704ec2cd
GitUser <120816634+Ursine09@users.noreply.github.com>
2024-12-14 14:00:39 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,123 @@
+package week4;
+ 
+public class Application {
+	static void aMethod() {
+		System.out.println("Yes!");
+		
+		 static void fullName (String fname, String lname);
+		    System.out.println(fname + lname);
+	}
+}
+
+public static void main(String[] args) {
+	
+	}
+//1
+		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
+int ageResult = ages[0] - ages[ages.length - 1];
+
+System.out.println(ageResult);
+
+// i
+int[] ages2 = new int [9];
+
+//ii	
+	int ageResult2 = ages2[0] - ages[ages.length -1];
+	
+	//iii
+	int ageResult1 = ages2[0] - ages[ages.length -1];
+	//c
+	public static double calculateAverage(int[] ages, int[] ages2) {
+        int totalSum = 0;
+        int totalLength = ages.length + ages2.length;
+
+        // Calculate sum of ages
+        for (int num : ages) {
+            totalSum += num;
+        }
+
+        // Calculate sum of ages2
+        for (int num : ages2) {
+            totalSum += num;
+        }
+
+        // Calculate average
+        return (double) totalSum / totalLength;
+
+	//2
+	String [] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"} ;
+	int index = 0; 
+
+	if (index >= 0 && index < names.length) {
+	    int length = names[index].length(); 
+	    int averageChar = length / names[index].length();
+	    System.out.println(averageChar);
+	    
+			//3 nameArray.length - 1
+	    
+			//4 nameArray(0)
+			//5
+	    int [] nameLengths;
+	    
+	    nameLengths = new int [6];
+	    
+	   //6
+	    
+	    for (int i = 0 ;i < names.length; i++)
+	    	for( String : names) {
+	    		System.out.println(names);
+	    	}
+	    
+	    
+			//7
+	    aMethod();
+	    aMethod();
+	    aMethod();
+			
+			//8
+	    String fname = "Raphael";
+	    String lname = "Williams";
+	    String fullName = fname + lname;
+	    
+	    System.out.println("fullName");
+	    
+	//9
+	    int [] variables = [6, 7, 8, 9] {
+	    	int sum = 0;
+	    	if (int sum)	    
+			
+			//10
+	    
+			//11
+			
+			
+			//12
+			double moneyInPocket = 9.50;
+	    
+			if (isHotOutside = true && moneyInPocket > 10.50)
+			
+			return isHotOutside = true;
+			
+	//13
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	} //end of main
+	public static int getAverage(int[] ages) {
+		   int sum = 0;
+		  for (int i=0; i<ages.length; i++) {
+		    sum += ages[i];
+		  }
+		  return sum / ages.length;
+		}
+	
+} //end of class
